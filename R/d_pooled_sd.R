4eae85f1ecc79d532439ffe615adbd9adb8580eb
vjilmari <vj.ilmarinen@gmail.com>
2020-12-22 11:42:11 +0200

    renamed d_pooled_sd function in the file of same name

--- R/d_pooled_sd.R
+++ R/d_pooled_sd.R
@@ -10,31 +10,31 @@
 #' @export
 #'
 #' @examples
-D.pooled.sd<-
+d_pooled_sd<-
   function(data,
            var,
            group.var,
            group.values,
            rename.output=TRUE){
-  
-  dat1<-data[data[,group.var]==group.values[1],] 
-  dat2<-data[data[,group.var]==group.values[2],] 
-  
+
+  dat1<-data[data[,group.var]==group.values[1],]
+  dat2<-data[data[,group.var]==group.values[2],]
+
   sd.1<-sd(dat1[,var])
   n.1<-length(dat1[,var])
-  
+
   sd.2<-sd(dat2[,var])
   n.2<-length(dat2[,var])
-  
+
   pooled.sd<-sqrt(((n.1-1)*sd.1^2+(n.2-1)*sd.2^2)/(n.1+n.2-2))
   diff<-mean(dat1[,var])-mean(dat2[,var])
   m.1<-mean(dat1[,var])
   m.2<-mean(dat2[,var])
   D<-diff/pooled.sd
   output<-cbind(n.1,n.2,m.1,m.2,sd.1,sd.2,pooled.sd,diff,D)
-  
+
   if (rename.output){
-    
+
     colnames(output)<-
       c(paste0("n.",group.values[1]),
         paste0("n.",group.values[2]),
@@ -46,8 +46,8 @@ D.pooled.sd<-
         "diff",
         "D")
   }
-  
-  
+
+
   return(output)
 }
 
