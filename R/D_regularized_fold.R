a9d013972ec284c0f794f35f05655af4ba9c3742
vjilmari <vj.ilmarinen@gmail.com>
2021-09-15 19:03:49 +0300

    Add D_regularized_fold function and colwise_pool helper

---
+++ R/D_regularized_fold.R
@@ -0,0 +1,123 @@
+#' Multivariate group difference estimation by regularization with sample folds
+#'
+#' @param data A data frame.
+#' @param mv.vars Character vector. Variable names in the multivariate variable set.
+#' @param group.var The name of the group variable.
+#' @param group.values Vector of length 2, group values (e.g. c("male", "female) or c(0,1)).
+#' @param alpha Alpha-value for penalizing function ranging from 0 to 1: 0 = ridge regression, 1 = lasso, 0.5 = elastic net (default).
+#' @param s Which lambda value is used for predicted values? Either "lambda.min" (default) or "lambda.1se".
+#' @param type.measure Which measure is used during cross-validation. Default "deviance".
+#' @param rename.output Logical. Should the output values be renamed according to the group.values? Default TRUE.
+#' @param fold.var Name of the fold variable.
+#'
+#' @return
+#' \item{D}{Multivariate descriptives and differences.}
+#' \item{pred.dat}{A data frame with predicted values.}
+#' @export
+#'
+#' @examples set.seed(34246)
+#' n1 <- 100
+#' n2 <- 10
+#' d <-
+#'   data.frame(
+#'     sex = sample(c("male", "female"), n1 * n2, replace = T),
+#'     fold = sample(x = 1:n2, size = n1 * n2, replace = T),
+#'     x1 = rnorm(n1 * n2),
+#'     x2 = rnorm(n1 * n2),
+#'     x3 = rnorm(n1 * n2)
+#'   )
+#'
+#' D_regularized_fold(
+#'   data = d,
+#'   mv.vars = c("x1", "x2", "x3"),
+#'   group.var = "sex",
+#'   group.values = c("female", "male"),
+#'   fold.var = "fold"
+#' )$D
+D_regularized_fold <-
+  function(data,
+           mv.vars,
+           group.var,
+           group.values,
+           alpha = 0.5,
+           s = "lambda.min",
+           type.measure = "deviance",
+           rename.output = TRUE,
+           fold.var) {
+    data$group.var.num <-
+      ifelse(data[, group.var] == group.values[1], 1,
+        ifelse(data[, group.var] == group.values[2], 0,
+          NA
+        )
+      )
+
+    fold.num.data <-
+      cbind.data.frame(
+        fold.num = c(1:length(unique(data[, fold.var]))),
+        fold = unique(data[, fold.var])
+      )
+
+    data <- dplyr::left_join(
+      x = data,
+      y = fold.num.data,
+      by = c("fold")
+    )
+
+    foldid <- data[, "fold.num"] # $fold.num
+
+    cv.mod <-
+      glmnet::cv.glmnet(
+        x = as.matrix(data[, c(mv.vars)]),
+        y = data$group.var.num,
+        family = c("binomial"),
+        foldid = foldid,
+        type.measure = type.measure,
+        alpha = alpha
+      )
+
+    preds <- data.frame(
+      group = data[, group.var],
+      fold = data[, "fold"],
+      pred = as.numeric(
+        stats::predict(cv.mod,
+          newx = as.matrix(data[, c(mv.vars)]),
+          s = s
+        )
+      )
+    )
+
+    D.folded <- list()
+    fold.names <- unique(preds[, "fold"])
+
+    for (i in fold.names) {
+      D.folded[[i]] <- multid::d_pooled_sd(
+        data = preds[preds$fold == i, ],
+        var = "pred",
+        group.var = "group",
+        group.values = group.values,
+        rename.output = FALSE
+      )
+    }
+
+    D.folded.df <- do.call(rbind.data.frame, D.folded)
+
+    D.folded.df <- cbind(
+      D.folded.df,
+      multid::colwise.pool(
+        data = D.folded.df,
+        n1 = "n.1",
+        n2 = "n.2",
+        m1 = "m.1",
+        m2 = "m.1",
+        sd1 = "sd.1",
+        sd2 = "sd.2"
+      )
+    )
+
+    D.folded.df$d.sd.total <- D.folded.df$diff /
+      D.folded.df$pooled.sd.total
+
+    comb.output <- list(D = D.folded.df, preds = preds, cv.mod = cv.mod)
+
+    return(comb.output)
+  }
---
+++ R/colwise_pool.R
@@ -0,0 +1,39 @@
+#' Column-wise pooling of standard deviations
+#'
+#' @param data Data frame of d_pooled_sd output for multiple samples.
+#' @param n1 Sample sizes of group1.
+#' @param n2 Sample sizes of group2.
+#' @param m1 Means of group1.
+#' @param m2 Means of group2.
+#' @param sd1 Standard deviations of group1.
+#' @param sd2 Standard deviations of group2.
+#'
+#' @return pooled SDs for groups and across groups
+#' @export
+#'
+#' @examples
+colwise.pool <- function(data, n1, n2, m1, m2, sd1, sd2) {
+  n1 <- data[, "n.1"]
+  n2 <- data[, "n.2"]
+  m1 <- data[, "m.1"]
+  m2 <- data[, "m.2"]
+  sd1 <- data[, "sd.1"]
+  sd2 <- data[, "sd.2"]
+
+  numerator1 <- (n1 - 1) * sd1^2
+  numerator2 <- (n2 - 1) * sd2^2
+  denominator1 <- (sum(n1) - length(n1))
+  denominator2 <- (sum(n2) - length(n2))
+
+  pooled.sd.1 <- sqrt(sum(numerator1) / denominator1)
+  pooled.sd.2 <- sqrt(sum(numerator2) / denominator2)
+
+  total.n1 <- sum(n1)
+  total.n2 <- sum(n2)
+
+  pooled.sd.total <- sqrt(((total.n1 - 1) * pooled.sd.1^2 +
+    (total.n2 - 1) * pooled.sd.2^2) /
+    (total.n1 + total.n2 - 2))
+  output <- cbind(pooled.sd.1, pooled.sd.2, pooled.sd.total)
+  return(output)
+}
