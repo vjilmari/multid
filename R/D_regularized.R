cc7682d7ef8d53d5274dc40914e39161c56126a5
vjilmari <vj.ilmarinen@gmail.com>
2021-09-15 10:59:57 +0300

    Tidyverse styling for functions.

--- R/D_regularized.R
+++ R/D_regularized.R
@@ -1,63 +1,70 @@
 #' Multivariate group difference estimation with regularized binomial regression
 #'
-#' @param data A data.frame
-#' @param mv.vars Character vector. Variable names in the multivariate variable set
-#' @param group.var The name of the group variable
-#' @param group.values Vector of length 2, group values (e.g. c("male", "female) or c(0,1))
-#' @param alpha Alpha-value for penalizing function ranging from 0 to 1: 0 = ridge regression, 1 = lasso, 0.5 = elastic net (default)
-#' @param nfolds Number of folds used for obtaining lambda (range from 3 to n-1, default 10)
-#' @param s Which lambda value is used for predicted values? Either "lambda.min" (default) or "lambda.1se"
-#' @param type.measure Which measure is used during cross-validation. Default "deviance"
+#' @param data A data frame.
+#' @param mv.vars Character vector. Variable names in the multivariate variable set.
+#' @param group.var The name of the group variable.
+#' @param group.values Vector of length 2, group values (e.g. c("male", "female) or c(0,1)).
+#' @param alpha Alpha-value for penalizing function ranging from 0 to 1: 0 = ridge regression, 1 = lasso, 0.5 = elastic net (default).
+#' @param nfolds Number of folds used for obtaining lambda (range from 3 to n-1, default 10).
+#' @param s Which lambda value is used for predicted values? Either "lambda.min" (default) or "lambda.1se".
+#' @param type.measure Which measure is used during cross-validation. Default "deviance".
 #' @param rename.output Logical. Should the output values be renamed according to the group.values? Default TRUE.
 #'
 #' @return
 #' \item{D}{Multivariate descriptives and differences}
-#' \item{pred.dat}{A data.frame with predicted values} 
+#' \item{pred.dat}{A data.frame with predicted values}
 #' @export
 #'
-#' @examples D_regularized(data = iris[iris$Species=="setosa" | iris$Species=="versicolor",],
-#'  mv.vars = c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width"), 
-#'  group.var="Species", group.values=c("setosa","versicolor"))$D
-
-D_regularized<-
+#' @examples D_regularized(
+#'   data = iris[iris$Species == "setosa" | iris$Species == "versicolor", ],
+#'   mv.vars = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
+#'   group.var = "Species", group.values = c("setosa", "versicolor")
+#' )$D
+D_regularized <-
   function(data,
            mv.vars,
            group.var,
            group.values,
-           alpha=0.5,
-           nfolds=10,
-           s="lambda.min",
-           type.measure="deviance",
-           rename.output=TRUE){
-    
-  data$group.var.num<-
-    ifelse(data[,group.var]==group.values[1],1,
-           ifelse(data[,group.var]==group.values[2],0,
-           NA))
-  
-  cv.mod<-
-    glmnet::cv.glmnet(x=as.matrix(data[,c(mv.vars)]),
-              y=data$group.var.num,
-              family=c("binomial"),
-              nfolds = nfolds,
-              type.measure=type.measure,
-              alpha=alpha)
-  
-  preds<-data.frame(
-    group=data[,group.var],
-    pred=as.numeric(
-      stats::predict(cv.mod,
-              newx=as.matrix(data[,c(mv.vars)]),
-              s=s)))
-  
-  D<-multid::d_pooled_sd(data = preds,
-                 var = "pred",
-                 group.var="group",
-                 group.values = group.values,
-                 rename.output = rename.output)
-  
-  comb.output<-list(D=D,pred.dat=preds)
-  return(comb.output)
-  
-}
+           alpha = 0.5,
+           nfolds = 10,
+           s = "lambda.min",
+           type.measure = "deviance",
+           rename.output = TRUE) {
+    data$group.var.num <-
+      ifelse(data[, group.var] == group.values[1], 1,
+        ifelse(data[, group.var] == group.values[2], 0,
+          NA
+        )
+      )
 
+    cv.mod <-
+      glmnet::cv.glmnet(
+        x = as.matrix(data[, c(mv.vars)]),
+        y = data$group.var.num,
+        family = c("binomial"),
+        nfolds = nfolds,
+        type.measure = type.measure,
+        alpha = alpha
+      )
+
+    preds <- data.frame(
+      group = data[, group.var],
+      pred = as.numeric(
+        stats::predict(cv.mod,
+          newx = as.matrix(data[, c(mv.vars)]),
+          s = s
+        )
+      )
+    )
+
+    D <- multid::d_pooled_sd(
+      data = preds,
+      var = "pred",
+      group.var = "group",
+      group.values = group.values,
+      rename.output = rename.output
+    )
+
+    comb.output <- list(D = D, pred.dat = preds)
+    return(comb.output)
+  }
--- R/d_pooled_sd.R
+++ R/d_pooled_sd.R
@@ -1,56 +1,56 @@
 #' Standardized mean difference with pooled standard deviation
 #'
-#' @param data A data.frame in long format
-#' @param var A continuous variable for which difference is estimated
-#' @param group.var The name of the group variable
-#' @param group.values Vector of length 2, group values (e.g. c("male", "female) or c(0,1))
+#' @param data A data frame.
+#' @param var A continuous variable for which difference is estimated.
+#' @param group.var The name of the group variable.
+#' @param group.values Vector of length 2, group values (e.g. c("male", "female) or c(0,1)).
 #' @param rename.output Logical. Should the output values be renamed according to the group.values? Default TRUE.
 #'
 #' @return Descriptive statistics and mean differences
 #' @export
 #'
 #' @examples
-#' d_pooled_sd(iris[iris$Species=="setosa" | iris$Species=="versicolor",],
-#'  var="Petal.Length", group.var="Species",
-#'   group.values=c("setosa","versicolor"))
-d_pooled_sd<-
+#' d_pooled_sd(iris[iris$Species == "setosa" | iris$Species == "versicolor", ],
+#'   var = "Petal.Length", group.var = "Species",
+#'   group.values = c("setosa", "versicolor")
+#' )
+d_pooled_sd <-
   function(data,
            var,
            group.var,
            group.values,
-           rename.output=TRUE){
+           rename.output = TRUE) {
+    dat1 <- data[data[, group.var] == group.values[1], ]
+    dat2 <- data[data[, group.var] == group.values[2], ]
 
-  dat1<-data[data[,group.var]==group.values[1],]
-  dat2<-data[data[,group.var]==group.values[2],]
+    sd.1 <- stats::sd(dat1[, var])
+    n.1 <- length(dat1[, var])
 
-  sd.1<-stats::sd(dat1[,var])
-  n.1<-length(dat1[,var])
+    sd.2 <- stats::sd(dat2[, var])
+    n.2 <- length(dat2[, var])
 
-  sd.2<-stats::sd(dat2[,var])
-  n.2<-length(dat2[,var])
+    pooled.sd <- sqrt(((n.1 - 1) * sd.1^2 + (n.2 - 1) * sd.2^2) / (n.1 + n.2 - 2))
+    diff <- mean(dat1[, var]) - mean(dat2[, var])
+    m.1 <- mean(dat1[, var])
+    m.2 <- mean(dat2[, var])
+    D <- diff / pooled.sd
+    output <- cbind(n.1, n.2, m.1, m.2, sd.1, sd.2, pooled.sd, diff, D)
 
-  pooled.sd<-sqrt(((n.1-1)*sd.1^2+(n.2-1)*sd.2^2)/(n.1+n.2-2))
-  diff<-mean(dat1[,var])-mean(dat2[,var])
-  m.1<-mean(dat1[,var])
-  m.2<-mean(dat2[,var])
-  D<-diff/pooled.sd
-  output<-cbind(n.1,n.2,m.1,m.2,sd.1,sd.2,pooled.sd,diff,D)
+    if (rename.output) {
+      colnames(output) <-
+        c(
+          paste0("n.", group.values[1]),
+          paste0("n.", group.values[2]),
+          paste0("m.", group.values[1]),
+          paste0("m.", group.values[2]),
+          paste0("sd.", group.values[1]),
+          paste0("sd.", group.values[2]),
+          "pooled.sd",
+          "diff",
+          "D"
+        )
+    }
 
-  if (rename.output){
 
-    colnames(output)<-
-      c(paste0("n.",group.values[1]),
-        paste0("n.",group.values[2]),
-        paste0("m.",group.values[1]),
-        paste0("m.",group.values[2]),
-        paste0("sd.",group.values[1]),
-        paste0("sd.",group.values[2]),
-        "pooled.sd",
-        "diff",
-        "D")
+    return(output)
   }
-
-
-  return(output)
-}
-
