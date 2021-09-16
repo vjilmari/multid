c5cf4dfda58d3c5606d7efd3a629b864b9ce2f02
vjilmari <vj.ilmarinen@gmail.com>
2021-09-16 10:45:14 +0300

    Start writing D_regularized_fold_out

---
+++ R/D_regularized_fold_out.R
@@ -0,0 +1,106 @@
+#' Use out-of-bag predictions across multiple folds.
+#'
+#' @param data A data frame.
+#' @param mv.vars Character vector. Variable names in the multivariate variable set.
+#' @param group.var The name of the group variable.
+#' @param group.values Vector of length 2, group values (e.g. c("male", "female) or c(0,1)).
+#' @param alpha Alpha-value for penalizing function ranging from 0 to 1: 0 = ridge regression, 1 = lasso, 0.5 = elastic net (default).
+#' @param nfolds Number of folds used for obtaining lambda (range from 3 to n-1, default 10).
+#' @param s Which lambda value is used for predicted values? Either "lambda.min" (default) or "lambda.1se".
+#' @param type.measure Which measure is used during cross-validation. Default "deviance".
+#' @param rename.output Logical. Should the output values be renamed according to the group.values? Default TRUE.
+#' @param size Integer. Size of regularization data per each group. Default 1/4 of cases.
+#' @param fold.var Name of the fold variable.
+#'
+#' @return
+#' \item{D}{Multivariate descriptives and differences in an out-of-bag dataset}
+#' \item{pred.dat}{A data.frame with predicted values in an out-of-bag dataset}
+#' @export
+#'
+#' @examples D_regularized_out(
+#'   data = iris[iris$Species == "setosa" |
+#'     iris$Species == "versicolor", ],
+#'   mv.vars = c(
+#'     "Sepal.Length", "Sepal.Width",
+#'     "Petal.Length", "Petal.Width"
+#'   ),
+#'   group.var = "Species",
+#'   group.values = c("setosa", "versicolor"),
+#'   size = 40
+#' )$D
+D_regularized_out <-
+  function(data,
+           mv.vars,
+           group.var,
+           group.values,
+           alpha = 0.5,
+           s = "lambda.min",
+           type.measure = "deviance",
+           rename.output = TRUE,
+           size = NULL,
+           fold.var) {
+    data$group.var.num <-
+      ifelse(data[, group.var] == group.values[1], 1,
+             ifelse(data[, group.var] == group.values[2], 0,
+                    NA
+             )
+      )
+
+    fold.num.data <-
+      cbind.data.frame(
+        fold.num = c(1:length(unique(data[, fold.var]))),
+        fold = unique(data[, fold.var])
+      )
+
+    data <- dplyr::left_join(
+      x = data,
+      y = fold.num.data,
+      by = c("fold")
+    )
+
+    if (is.null(size)){
+      size=round((nrow(data)/length(unique(data[, fold.var])))4,0)} else {size=size}
+
+    data$row.nmbr <- rownames(data)
+
+    data.grouped <- dplyr::group_by(data, group.var.num)
+
+    train.data <- dplyr::sample_n(data.grouped,
+                                  size = size,
+                                  replace = F
+    )
+
+    test.data <- data[!(data$row.nmbr %in% train.data$row.nmbr), ]
+    train.data <- dplyr::ungroup(train.data)
+
+    cv.mod <-
+      glmnet::cv.glmnet(
+        x = as.matrix(train.data[, c(mv.vars)]),
+        y = train.data$group.var.num,
+        family = c("binomial"),
+        nfolds = nfolds,
+        type.measure = type.measure,
+        alpha = alpha
+      )
+
+    preds <- data.frame(
+      group = test.data[, group.var],
+      pred = as.numeric(
+        stats::predict(cv.mod,
+                       newx = as.matrix(test.data[, c(mv.vars)]),
+                       s = s
+        )
+      )
+    )
+
+    D <- multid::d_pooled_sd(
+      data = preds,
+      var = "pred",
+      group.var = "group",
+      group.values = group.values,
+      rename.output = rename.output
+    )
+
+    comb.output <- list(D = D, pred.dat = preds)
+    return(comb.output)
+  }
