3304cc7f1d000401b7bc8d028e2f2f98e4f7b2da
vjilmari <vj.ilmarinen@gmail.com>
2025-09-23 09:19:22 +0300

    Add documentation for the big5_example fallback example datafile

---
+++ R/data_documentation.R
@@ -0,0 +1,18 @@
+#' Example BIG5 personality dataset (random subset)
+#'
+#' A small subset of the BIG5 dataset from OpenPsychometrics, included as a
+#' fallback for vignettes. This dataset consists of 200 U.S. respondents:
+#' 100 men and 100 women.
+#'
+#' @format A data frame with 200 rows and the same variables as the full BIG5 dataset.
+#' \describe{
+#'   \item{gender}{Gender of respondent (1 = male, 2 = female)}
+#'   \item{country}{Country of respondent (here always "US")}
+#'   \item{age}{Age of respondent}
+#'   \item{N1-C10}{Personality items}
+#'
+#' }
+#'
+#' @source \url{http://openpsychometrics.org/_rawdata/}
+#'
+"example_big5"
